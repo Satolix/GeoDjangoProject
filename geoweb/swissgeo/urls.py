0809c68590b57d36c6660b181dd29a7befc6cc1a
janis.badertsc <janis.badertscher@students.hevs.ch>
2024-06-23 13:10:29 +0200

    changed html from slops.html to map.html and also the paths in the url and the view. Added liftsjson and buldingsjson to the path

--- geoweb/swissgeo/urls.py
+++ geoweb/swissgeo/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('map/', views.map, name='slopes'),
-    path('slopes.json', views.slopesjson, name='slopesjson')
+    path('slopes.json', views.slopesjson, name='slopesjson'),
+    path('lifts.json', views.liftsjson, name='liftsjson'),
+    path('buildings.json', views.buildingsjson, name='buildingsjson'),
 ]
--- geoweb/swissgeo/views.py
+++ geoweb/swissgeo/views.py
@@ -2,16 +2,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.core.serializers import serialize
-from .models import Slope
+from .models import Building, Lift, Slope
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. It works!")
 
 def map(request):
-    return render(request, 'slopes.html')
+    return render(request, 'map.html')
 
 def slopesjson(request):
     slopes_data = Slope.objects.all()
     serializer = serialize('geojson', slopes_data, geometry_field='geom', fields=('name', 'difficulty'))
+    return HttpResponse(serializer, content_type='application/json')
+
+def liftsjson(request):
+    lifts_data = Lift.objects.all()
+    serializer = serialize('geojson', lifts_data, geometry_field='geom', fields=('name', 'capacity'))
+    return HttpResponse(serializer, content_type='application/json')
+
+def buildingsjson(request):
+    buildings_data = Building.objects.all()
+    serializer = serialize('geojson', buildings_data, geometry_field='geom', fields=('name', 'type'))
     return HttpResponse(serializer, content_type='application/json')
