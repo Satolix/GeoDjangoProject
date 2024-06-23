fe257cfbcf04ea8da2e7b15bdd9c8154b7199ee4
esvaria <marie-esther.mabillard@students.hevs.ch>
2024-06-23 15:28:41 +0200

    migration for slopes

--- geoweb/swissgeo/models.py
+++ geoweb/swissgeo/models.py
@@ -7,6 +7,7 @@ class Slope(models.Model):
     name = models.CharField(max_length=100)
     difficulty = models.CharField(max_length=100, choices=[('green', 'green'), ('blue', 'blue'), ('red', 'red'), ('black', 'black')], default='green')
     status = models.CharField(max_length=100, choices=[('open', 'open'), ('closed', 'closed')], default='closed')
+    type = models.CharField(max_length=100, choices=[('chairlift', 'chairlift'), ('gondola', 'gondola'), ('teleski', 'teleski')], default='chairlift')
     geom = models.LineStringField()
 
     class Meta:
@@ -24,6 +25,7 @@ class Lift(models.Model):
     name = models.CharField(max_length=100)
     capacity = models.IntegerField(default=2)
     geom = models.LineStringField()
+    status = models.CharField(max_length=100, choices=[('open', 'open'), ('closed', 'closed')], default='closed')
 
     class Meta:
         db_table = 'lifts'
